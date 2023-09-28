757257c259a388c0d172187955d1709dcf86b4fc
birkirfreyrg <birkirfrey@msn.com>
2023-09-28 22:25:42 +0000

    database for check-in check-out and guestsNumber

---
+++ pages/api/addGuest.js
@@ -0,0 +1,24 @@
+// pages/api/addGuest.js
+import { mongooseConnect } from "../../lib/mongoose"; // Import the MongoDB connection
+import { Guests } from "../../models/Guests"; // Import the schema/model
+
+export default async function handle(req, res) {
+  if (req.method !== "POST") {
+    res.json("should be a POST request");
+    return;
+  }
+  const { checkInDate, checkOutDate, numberOfGuests } = req.body;
+
+  await mongooseConnect();
+  try {
+    const GuestsBooked = await Guests.create({
+      checkInDate,
+      checkOutDate,
+      numberOfGuests,
+    });
+    res.status(200).json({ message: "Success" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
---
+++ pages/api/fetchData.js
@@ -0,0 +1,12 @@
+import { mongooseConnect } from "../../lib/mongoose"; // Import the MongoDB connection
+import { Dates } from "../../models/Dates"; // Import the schema/model
+
+export default async (req, res) => {
+  try {
+    const data = await Dates.find(); // Fetch data from the database
+    res.status(200).json(data); // Send the data as a JSON response
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
