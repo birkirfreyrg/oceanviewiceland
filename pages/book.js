445e6f5c5f0febbbb73de101f242420379ba693a
birkirfreyrg <birkirfrey@msn.com>
2024-03-09 18:28:18 +0000

    removed shardeData from book

--- pages/book.js
+++ pages/book.js
@@ -1,8 +1,5 @@
 import FinishBooking from "./FinishBooking";
-import { useContext } from "react";
-import { MyContext } from "../contexts/MyContext";
 
 export default function book() {
-  const { sharedData } = useContext(MyContext);
   return <FinishBooking />;
 }
